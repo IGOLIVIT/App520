0b75024c2f036c677b96dacf3a942c863cb5b5e3
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-07-22 12:24:00 +0300

    Initial Commit

---
+++ App520/App520App.swift
@@ -0,0 +1,17 @@
+//
+//  App520App.swift
+//  App520
+//
+//  Created by IGOR on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct App520App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
